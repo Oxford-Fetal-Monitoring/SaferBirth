f9f5f1529b49e67b13225cf34807237092632a0b
Omnisciens3 <kristian.georgi3v@protonmail.com>
2024-05-13 18:30:58 +0100

    removed some of the information in the mission section. Updated the JS so now we have animations when scrolling. Enhanced the logo in the navbar

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const burger = document.querySelector('.burger');
     const navLinks = document.getElementById('nav-links');
     const links = document.querySelectorAll('#nav-links a');  // Get all nav links
+    var controller = new ScrollMagic.Controller();
 
     burger.addEventListener('click', function() {
         navLinks.classList.toggle('nav-active');
@@ -17,4 +18,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
         burger.classList.remove('toggle');
     }));
+
+    // Create a scene for when the user scrolls to the end of the hero section
+    new ScrollMagic.Scene({
+        triggerElement: "#home", // point of execution
+        duration: "100%", // pin the element for the window height - 1
+        triggerHook: 0 // don't trigger until #home hits the top of the viewport
+    })
+    .setTween(gsap.timeline()
+        .fromTo("#home", {opacity: 1}, {opacity: 0}, "start")  // Fade out the hero section
+        .fromTo("#mission", {opacity: 0}, {opacity: 1}, "start+=0.001")  // Fade in the mission section
+    )
+    .addTo(controller);  // assign the scene to the controller
+
+     // Create a timeline for sequential animations
+     var missionTimeline = gsap.timeline();
+
+     // Fade in the intro
+     missionTimeline.fromTo("#mission .intro", {opacity: 0}, {opacity: 1, duration: 0.5});
+ 
+     // Fade in each arrow-section sequentially
+     document.querySelectorAll("#mission .arrow-section").forEach((section, index) => {
+         missionTimeline.fromTo(section, {opacity: 0}, {opacity: 1, duration: 0.5}, `+=0.5`);
+     });
+ 
+     // Create a ScrollMagic scene
+     new ScrollMagic.Scene({
+         triggerElement: "#mission",
+         triggerHook: 0.5,  // Trigger when #mission is halfway into the viewport
+         reverse: false      // Optionally, do not reverse the animation when scrolling back up
+     })
+     .setTween(missionTimeline)
+     .addTo(controller);
 });
